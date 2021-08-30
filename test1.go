4266ff6a8618bf2d31fa0d5046a14add112532aa
rajatsaini736 <rajatsaini736@gmail.com>
2021-08-30 22:11:50 +0530

    basic CRUD on slice

--- test1.go
+++ test1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -25,8 +26,9 @@ func getRolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func getRoll(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")	
 	params := mux.Vars(r)
+	
 	for _, item := range rolls {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
@@ -37,33 +39,62 @@ func getRoll(w http.ResponseWriter, r *http.Request) {
 
 func createRoll(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	
 	var newRoll Roll
-	json.NewDecoder(r.Body).Decode(&newroll)
+	json.NewDecoder(r.Body).Decode(&newRoll)
 	newRoll.ID = strconv.Itoa(len(rolls) + 1)
+
+	rolls = append(rolls, newRoll)
+	
+	json.NewEncoder(w).Encode(newRoll)
 }
 
 func updateRoll(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, item := range(rolls) {
+		if item.ID == params["id"] {
+			rolls = append(rolls[:i], rolls[i+1:]...)
+		}
+		var newRoll Roll
+		
+		json.NewDecoder(r.Body).Decode(&newRoll)
+		newRoll.ID = params["id"]
+		rolls = append(rolls, newRoll)
+		
+		json.NewEncoder(w).Encode(rolls)
+		return
+	}
 }
 
 func deleteRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	params := mux.Vars(r)
+	for i, item := range(rolls) {
+		if item.ID == params["id"] {
+			rolls = append(rolls[:i], rolls[i+1:]...)
+			json.NewEncoder(w).Encode(rolls)
+			return
+		}
+	}
 }
 
 func main() {
-	// newRoll := Roll{ID: "123", ImageNumber: "imageNum", Name: "Thomos", Ingredients: "Shelby"}
-	// rolls = append(rolls, newRoll)
-	// fmt.Println(rolls)
-
+	// initialize slice with slice literal
+	newRoll1 := Roll{ID: "123", ImageNumber: "imageNum", Name: "Thomos", Ingredients: "Shelby"}
+	newRoll2 := Roll{ID: "234", ImageNumber: "imageNum1", Name: "Arthur", Ingredients: "Shelby Bros"}
+	rolls = append(rolls, newRoll1, newRoll2)
+	fmt.Println(rolls)
 	// initialize router
 	router := mux.NewRouter()
 
 	//endpoints
 	router.HandleFunc("/sushi", getRolls).Methods("GET")
 	router.HandleFunc("/sushi/{id}", getRoll).Methods("GET")
-	router.HandleFunc("/sushi", createRoll).Methods("POSTw")
-	router.HandleFunc("/shushi/{id}", updateRoll).Methods("POST")
-	router.HandleFunc("/shushi/{id}", deleteRoll).Methods("DELETE")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", deleteRoll).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 
